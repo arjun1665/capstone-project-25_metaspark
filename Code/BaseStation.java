1dbed2d6d790a9aa9c041ec5f0ddc4498ff4cd1d
KARTHIK P <itskp2004@gmail.com>
2025-09-15 02:14:15 +0530

    Update BaseStation.java
    Update BaseStation.java by adding location, range, cost, and deadline-aware task assignment

--- Code/BaseStation.java
+++ Code/BaseStation.java
@@ -2,24 +2,48 @@ package org.fog.marina;
 
 public class BaseStation {
     private String id;
+    private double x;
+    private double y;
     private double cpuCapacity;
     private double storage;
+    private double range; 
 
-    public BaseStation(String id, double cpuCapacity, double storage) {
+  
+    private static final double COST_FACTOR = 11.444;  
+
+    public BaseStation(String id, double x, double y, double cpuCapacity, double storage, double range) {
         this.id = id;
+        this.x = x;
+        this.y = y;
         this.cpuCapacity = cpuCapacity;
         this.storage = storage;
+        this.range = range;
     }
 
     public String getId() { return id; }
     public double getCpuCapacity() { return cpuCapacity; }
     public double getStorage() { return storage; }
+    public double getX() { return x; }
+    public double getY() { return y; }
+    public double getRange() { return range; }
 
+    public void setCpuCapacity(double cpuCapacity) { this.cpuCapacity = cpuCapacity; }
+
+   
     public boolean canProcess(Task task) {
         return task.getCpu() <= cpuCapacity && task.getSize() <= storage;
     }
 
-    public void assignTask(Task task) {
-        System.out.println("Task " + task.getId() + " assigned to Base Station " + id);
+
+    public void assignTask(Task task, double currentTime) {
+        double procTime = task.computeProcessingTime(cpuCapacity);
+        double finishTime = currentTime + procTime;
+        task.setFinishTime(finishTime);
+        double cost = task.getCpu() * COST_FACTOR;
+        task.setCost(cost);
+
+        System.out.println("Task " + task.getId() + " assigned to Base Station " + id +
+                           " [Finish=" + finishTime + ", Deadline=" + (task.getArrivalTime() + task.getDeadline()) +
+                           ", Cost=" + cost + "]");
     }
 }
